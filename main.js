79347805e8dc53da0102863eacc61036f5f1db85
Tijn Kersjes <tijn@divbyzero.nl>
2018-07-31 18:55:45 +0200

    Add RenderSystem

--- main.js
+++ main.js
@@ -33,12 +33,26 @@ const createWorld = () => {
 }
 
 const hasComponent = cname => ent => !!ent.components[cname.name || cname]
+const hasComponents = cnames => ent => cnames.every(cname => !!ent.components[cname.name || cname])
+
+
+/* ---------- Actual Game Logic starts here ---------- */
 
 function LogComponent(msg) {
   this.message = msg
 }
 
 function WarnComponent() {}
+
+function SpriteComponent(size, color) {
+  this.size = size
+  this.color = color
+}
+
+function PositionComponent(x, y) {
+  this.x = x
+  this.y = y
+}
 
 const LogSystem = ents => ents
   .filter(hasComponent(LogComponent))
@@ -49,6 +63,22 @@ const LogSystem = ents => ents
       console.log(x.components.LogComponent.message);
     delete x.components.LogComponent
   })
+
+const RenderSystem = (canvas, w, h) => {
+  const ctx = canvas.getContext('2d')
+  canvas.width = w
+  canvas.height = h
+
+  return ents => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    ents
+      .filter(hasComponents([SpriteComponent, PositionComponent]))
+      .forEach(ent => {
+        ctx.fillStyle = ent.components.SpriteComponent.color
+        ctx.fillRect(ent.components.PositionComponent.x, ent.components.PositionComponent.y, ent.components.SpriteComponent.size, ent.components.SpriteComponent.size)
+      })
+    }
+}
 
 window.addEventListener('load', () => {
 
@@ -61,6 +91,16 @@ window.addEventListener('load', () => {
     .addComponent(new LogComponent('world!'))
     .addComponent(new WarnComponent)
 
+  world.createEntity()
+    .addComponent(new PositionComponent(10, 10))
+    .addComponent(new SpriteComponent(32, '#ff00ff'))
+
+  world.createEntity()
+    .addComponent(new PositionComponent(250, 250))
+    .addComponent(new SpriteComponent(32, '#00ffff'))
+
+  const canvas = document.getElementById('cvs')
+  world.addSystem(RenderSystem(canvas, 400, 300))
   world.addSystem(LogSystem)
 
   const gameLoop = () => {
